b9c70c844b9a35c8a52e883c3303e009a3aa69cf
monkeychen <cza55007@gmail.com>
2018-06-26 01:45:42 +0800

    add first scrapy demo

---
+++ chapter1/movie/run.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+from scrapy.cmdline import execute
+
+
+cur_dir_name = os.path.dirname(os.path.abspath(__file__))
+print("current directory name = %s" % cur_dir_name)
+sys.path.append(cur_dir_name)
+
+execute(["scrapy", "crawl", "meiju"])
+
