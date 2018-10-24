9f66a70ac69d09d9838f8de55629e86de49a81b9
monkeychen <cza55007@gmail.com>
2018-10-24 23:48:46 +0800

    增加leetcode解题代码

---
+++ leetcode/problems/AddTwoNums.py
@@ -0,0 +1,102 @@
+"""
+给定两个非空链表来表示两个非负整数。位数按照逆序方式存储，它们的每个节点只存储单个数字。将两数相加返回一个新的链表。
+
+你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+
+示例：
+
+输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 0 -> 8
+原因：342 + 465 = 807
+"""
+import json
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        tail1, tail2, ptr = l1, l2, ListNode(0)
+        result = ptr
+        while tail1 is not None or tail2 is not None:
+            val = ptr.val
+            if tail1 is not None and tail2 is None:
+                val, tail1 = val + tail1.val, tail1.next
+            elif tail1 is None and tail2 is not None:
+                val, tail2 = val + tail2.val, tail2.next
+            else:
+                val, tail1, tail2 = val + tail1.val + tail2.val, tail1.next, tail2.next
+
+            ptr.val, carry = val % 10, val // 10
+            if carry > 0 or tail1 is not None or tail2 is not None:
+                ptr.next = ListNode(carry)
+                ptr = ptr.next
+
+        return result
+
+
+def stringToIntegerList(input):
+    return json.loads(input)
+
+
+def stringToListNode(input):
+    # Generate list from the input
+    numbers = stringToIntegerList(input)
+
+    # Now convert that list into linked list
+    dummyRoot = ListNode(0)
+    ptr = dummyRoot
+    for number in numbers:
+        ptr.next = ListNode(number)
+        ptr = ptr.next
+
+    ptr = dummyRoot.next
+    return ptr
+
+
+def listNodeToString(node):
+    if not node:
+        return "[]"
+
+    result = ""
+    while node:
+        result += str(node.val) + ", "
+        node = node.next
+    return "[" + result[:-2] + "]"
+
+
+def main():
+    import sys
+    import io
+
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            l1 = stringToListNode(line)
+            line = next(lines)
+            l2 = stringToListNode(line)
+
+            ret = Solution().addTwoNumbers(l1, l2)
+
+            out = listNodeToString(ret)
+            print(out)
+        except StopIteration:
+            break
+
+
+if __name__ == '__main__':
+    main()
